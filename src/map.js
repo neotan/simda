4e3df5b3c4fd248a2eeaf61c83e8cf9d6a5dcab9
Your Name <you@example.com>
2020-01-21 19:49:25 +0800

    added map()

---
+++ src/map.js
@@ -0,0 +1,62 @@
+import curry2 from "./curry2";
+import isPlainObj from "./isPlainObj";
+import _toString from "./internal/_toString";
+import isFunction from "./isFunction";
+
+const ARRAYS = [
+  '[object Array]',
+  '[object Int8Array]',
+  '[object Uint8Array]',
+  '[object Uint8ClampedArray]',
+  '[object Int16Array]',
+  '[object Uint16Array]',
+  '[object Int32Array]',
+  '[object Uint32Array]',
+  '[object Float32Array]',
+  '[object Float64Array]',
+]
+
+const execFn = (val, key, fn) => (isFunction(val) ? val : fn(val, key))
+
+const map = (fn, functor) => {
+  let tempFunctor = new functor.constructor()
+  switch (true) {
+    case isPlainObj(functor):
+      tempFunctor = new functor.constructor()
+      return Object.entries(functor).reduce((accu, [key, val]) => {
+        accu[key] = execFn(val, key, fn)
+        return accu
+      }, {})
+
+    case ARRAYS.includes(_toString(functor)):
+      tempFunctor = new functor.constructor(functor.length)
+      functor.forEach((val, i) => {
+        tempFunctor[i] = execFn(val, i, fn)
+      })
+      return tempFunctor
+
+    case _toString(functor) === '[object Set]':
+      Object.entries(functor).forEach((val, key) => {
+        tempFunctor.add(execFn(val, key), fn)
+      })
+      return tempFunctor
+
+    case _toString(functor) === '[object Map]':
+      Object.entries(functor).forEach((val, key) => {
+        tempFunctor.set(key, execFn(val, key), fn)
+      })
+      return tempFunctor
+
+    case _toString(functor) === '[object String]':
+      tempFunctor = ''
+      Object.entries(functor).forEach(([i, char]) => {
+        tempFunctor += execFn(char, i, fn)
+      })
+      return tempFunctor
+
+    default:
+      return functor
+  }
+}
+
+export default curry2(map)
---
+++ tests/map.test.js
@@ -0,0 +1,51 @@
+import assert from "assert";
+import eq from "./shared/eq";
+import map from "../src/map";
+
+describe('map', () => {
+  const times2 = x => {
+    return x * 2
+  }
+  const add1 = x => {
+    return x + 1
+  }
+  const dec = x => {
+    return x - 1
+  }
+
+  it('maps simple functions over arrays', () => {
+    eq(map(times2, [1, 2, 3, 4]), [2, 4, 6, 8])
+  })
+
+  it('maps simple functions into arrays', () => {
+    eq(map(times2)([1, 2, 3, 4]), [2, 4, 6, 8])
+  })
+
+  it('maps over objects', () => {
+    eq(map(dec, {}), {})
+    eq(map(dec, { x: 4, y: 5, z: 6 }), { x: 3, y: 4, z: 5 })
+  })
+
+  it('dispatches to objects that implement `map`', () => {
+    const obj = {
+      x: 100,
+      map: f => f(this.x),
+    }
+    eq(map(add1, obj), { x: 101, map: f => f(this.x) })
+  })
+
+  it('throws a TypeError on null and undefined', () => {
+    assert.throws(() => {
+      return map(times2, null)
+    }, TypeError)
+    assert.throws(() => {
+      return map(times2, undefined)
+    }, TypeError)
+  })
+
+  it('composes', () => {
+    const mdouble = map(times2)
+    const mdec = map(dec)
+    eq(mdec(mdouble([10, 20, 30])), [19, 39, 59])
+  })
+})
