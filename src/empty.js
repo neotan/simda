f1a46cb13d12974b75e51da88f52b1aac28eff9e
Your Name <you@example.com>
2020-02-03 17:35:02 +0800

    added empty()

---
+++ src/empty.js
@@ -0,0 +1,23 @@
+import _isArguments from './internal/_isArguments'
+import _isArray from './internal/_isArray'
+import _isObject from './internal/_isObject'
+import _isString from './internal/_isString'
+
+const empty = x => {
+  switch (true) {
+    case _isArray(x):
+      return []
+    case _isString:
+      return ''
+    case _isObject(x):
+      return {}
+    case _isArguments(x):
+      // prettier-ignore
+      // eslint-disable-next-line
+      return (function() {return arguments;})()
+    default:
+      return undefined
+  }
+}
+
+export default empty
