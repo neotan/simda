5dc44f9b825ba3d9ee16a442749bfc9b0671195e
Your Name <you@example.com>
2020-02-05 01:56:13 +0800

    reformatted

--- tests/replace.test.js
+++ tests/replace.test.js
@@ -2,13 +2,11 @@ import { replace } from '../src'
 import eq from './shared/eq'
 
 describe('replace', function() {
-
   it('replaces substrings of the input string', function() {
-    eq(replace('1', 'one', '1 two three'), 'one two three');
-  });
+    eq(replace('1', 'one', '1 two three'), 'one two three')
+  })
 
   it('replaces regex matches of the input string', function() {
-    eq(replace(/\d+/g, 'num', '1 2 three'), 'num num three');
-  });
-
-});
+    eq(replace(/\d+/g, 'num', '1 2 three'), 'num num three')
+  })
+})
