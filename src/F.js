b6157576d89f3128b15cf7833567956d860fd866
Your Name <you@example.com>
2020-02-05 23:30:11 +0800

    removed _curry1 on F() and T()

--- src/F.js
+++ src/F.js
@@ -1,6 +1,5 @@
 import always from './always'
-import _curry1 from './internal/_curry1'
 
 const F = always(false)
 
-export default _curry1(F)
+export default F
--- src/T.js
+++ src/T.js
@@ -1,6 +1,5 @@
 import always from './always'
-import _curry1 from './internal/_curry1'
 
 const T = always(true)
 
-export default _curry1(T)
+export default T
